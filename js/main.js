e9021a46d5a77f6cbfe781a8fa96b45298bb468a
Gennady Yudin <soulmatesneverdie92@gmail.com>
2016-12-21 07:16:52 +0300

    old
    old

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/**
+ * Created by slowdive on 08.10.2016.
+ */
+$(".volsu").mouseover(function () {
+    $(".career__pic").css({"background-image": "url(/assets/volsu2.png)", "background-size": "contain"})
+});
+
+$(".ped").mouseover(function () {
+    $(".career__pic").css({"background-image": "url(/assets/ped.png)", "background-size": "contain"})
+});
+
+$(".citilink").mouseover(function () {
+    $(".career__pic").css({"background-image": "url(/assets/job.jpg)", "background-size": "cover"})
+});
+
+$(".loftschool").mouseover(function () {
+    $(".career__pic").css({"background-image": "url(/assets/photo.jpg)", "background-size": "cover"})
+});
